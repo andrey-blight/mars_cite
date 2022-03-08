d507e26e4c2a144d1531b02d07f3065ddaeee299
Kizhinov-Andrey <kzihinov05@gmail.com>
2022-03-08 18:16:04 +0300

    Add sqlalchemy

---
+++ data/jobs.py
@@ -0,0 +1,15 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    job = sqlalchemy.Column(sqlalchemy.String)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer)
+    collaborators = sqlalchemy.Column(sqlalchemy.String)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
---
+++ data/users.py
@@ -0,0 +1,17 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    age = sqlalchemy.Column(sqlalchemy.Integer)
+    position = sqlalchemy.Column(sqlalchemy.String)
+    speciality = sqlalchemy.Column(sqlalchemy.String)
+    address = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
