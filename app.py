2589071265614baedc80a4f9e69e37c07c9862bb
Kizhinov-Andrey <kzihinov05@gmail.com>
2022-03-22 15:34:02 +0300

    Edit bugs with rest api

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from data.jobs import Jobs
 from data.departments import Departments
 from data.forms import *
 from data.category import Category
+from data import jobs_api
 
 from flask import Flask, render_template, redirect, request
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 global_init(r"db/mars_explorer.db")
 app = Flask(__name__)
+app.register_blueprint(jobs_api.blueprint)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
--- data/jobs.py
+++ data/jobs.py
@@ -3,10 +3,11 @@ from .category import association_table
 
 import datetime
 import sqlalchemy
+from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy import orm
 
 
-class Jobs(SqlAlchemyBase):
+class Jobs(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'jobs'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
